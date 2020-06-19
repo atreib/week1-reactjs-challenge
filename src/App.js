9865b8bfacee848bbc38b4f6ebd69864be91f511
André Treib <andre.schumacher.treib@gmail.com>
2020-06-18 23:19:01 -0300

    end of challenge

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
-import React from "react";
-
+import React, { useState, useEffect } from "react";
+import api from './services/api';
 import "./styles.css";
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  const getRepositories = async () => {
+    const { data } = await api.get("repositories");
+    setRepositories(data);
+  }
+
+  useEffect(() => {
+    getRepositories();
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+    const dateId = Date.now();
+    const newRepo = {
+      title: `teste ${dateId}`,
+      url: `teste url ${dateId}`,
+      techs: [`teste tech ${dateId}`, `teste tech 2${dateId}`]
+    };
+    const { data } = await api.post('repositories', newRepo);
+    setRepositories([...repositories, data]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete(`repositories/${id}`);
+    setRepositories(repositories.filter(item => item.id !== id))
+  }
+
+  async function handleLikeRepository(id) {
+    await api.post(`repositories/${id}/like`);
+    await getRepositories();
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {repositories.map(repo => {
+          return (
+            <li key={repo.id}>
+              [{repo.likes} LIKES] <b style={{padding: '8px'}}> {repo.title} </b> ({repo.url})
+              <button className="btn-primary" onClick={() => handleLikeRepository(repo.id)}>
+                Curtir
+              </button>
+              <button onClick={() => handleRemoveRepository(repo.id)}>
+                Remover
+              </button>
+            </li>
+          );
+        })}
+        
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
